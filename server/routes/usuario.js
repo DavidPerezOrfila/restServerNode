de97d5a830a03f0601941a07f98a365639289575
David Pérez <knightbcn@gmail.com>
2018-08-08 10:27:03 +0200

    modify server.js

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,46 @@
+// express
 const express = require('express');
 const app = express();
+
+// Encriptador de passwords
 const bcrypt = require('bcrypt');
+
+// Paquete con librerías de ayuda (map, filter...)
+const _ = require('underscore');
+
+// Llama a la plantilla de usuario
 const Usuario = require('../models/usuario');
 
 app.get('/usuario', function(req, res) {
-    res.json('getUsuario');
+
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    // Pasando los campos como segundo parámetro en la función find Usuario.find({}, 'nombre', 'email')
+    // Excluiría el resto de campos
+    Usuario.find({ estado: true })
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            // Devuelve un contador con los usuarios mostrados
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+            });
+        });
 });
 
 app.post('/usuario', function(req, res) {
@@ -35,7 +71,7 @@ app.post('/usuario', function(req, res) {
 
 app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    let body = req.body;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
     Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
         if (err) {
@@ -51,8 +87,29 @@ app.put('/usuario/:id', function(req, res) {
     });
 });
 
-app.delete('/usuario', function(req, res) {
-    res.json('deleteUsuario');
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    let cambiaEstado = {
+        estado: false
+    }
+
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => { <--(borra físicamente el registro)
+    // Cambiamos el estado a false
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err || !usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    });
 });
 
 module.exports = app;
